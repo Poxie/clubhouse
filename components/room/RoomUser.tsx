e9907e365f31f245038f3198b1e188a764475548
Poxie <poxen12@hotmail.com>
2022-07-16 07:07:15 +0200

    Determining whether a user is speaking or not

--- components/room/RoomUser.tsx
+++ components/room/RoomUser.tsx
@@ -1,15 +1,35 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useConnection } from '../../contexts/ConnectionProvider';
 import styles from '../../styles/Room.module.scss';
+import hark from 'hark';
 
 export const RoomUser: React.FC<{
     uid: string;
     displayName: string;
     photoURL: string;
 }> = ({ uid, displayName, photoURL }) => {
+    const [talking, setTalking] = useState(false);
+    const { remoteStreams } = useConnection();
+    const remoteStream = remoteStreams[uid];
+
+    // Determining if user is speaking
+    useEffect(() => {
+        if(!remoteStream) return;
+
+        const speechEvents = hark(remoteStream);
+
+        speechEvents.on('speaking', () => setTalking(true));
+        speechEvents.on('stopped_speaking', () => setTalking(false));
+    }, [remoteStream]);
+    
+    const iconClassName = [
+        styles['user-icon'],
+        talking ? styles['talking'] : ''
+    ].join(' ');
     return(
         <div className={styles['user']}>
-            <div className={styles['user-icon']}>
+            <div className={iconClassName}>
                 <Image 
                     src={photoURL}
                     layout={'fill'}
