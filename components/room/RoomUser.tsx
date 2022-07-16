59a63501c525a942b46b5174f8721f8e65cfde56
Poxie <poxen12@hotmail.com>
2022-07-16 07:08:28 +0200

    Adding speaking indicator for self-user

--- components/room/RoomUser.tsx
+++ components/room/RoomUser.tsx
@@ -3,25 +3,29 @@ import React, { useEffect, useState } from 'react';
 import { useConnection } from '../../contexts/ConnectionProvider';
 import styles from '../../styles/Room.module.scss';
 import hark from 'hark';
+import { useAppSelector } from '../../redux/store';
+import { selectUserId } from '../../redux/user/hooks';
 
 export const RoomUser: React.FC<{
     uid: string;
     displayName: string;
     photoURL: string;
 }> = ({ uid, displayName, photoURL }) => {
+    const userId = useAppSelector(selectUserId);
     const [talking, setTalking] = useState(false);
-    const { remoteStreams } = useConnection();
+    const { remoteStreams, localStream } = useConnection();
     const remoteStream = remoteStreams[uid];
 
     // Determining if user is speaking
     useEffect(() => {
-        if(!remoteStream) return;
+        const stream = userId === uid ? localStream : remoteStream;
+        if(!stream) return;
 
-        const speechEvents = hark(remoteStream);
+        const speechEvents = hark(stream);
 
         speechEvents.on('speaking', () => setTalking(true));
         speechEvents.on('stopped_speaking', () => setTalking(false));
-    }, [remoteStream]);
+    }, [remoteStream, localStream]);
     
     const iconClassName = [
         styles['user-icon'],
